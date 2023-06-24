57c2940e17536811093e771e81fb8028d32aa157
AndRadulescu05 <radulescu1991@gmail.com>
2023-06-24 16:41:16 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello! This is my GitHub profile.");
         System.out.println("Welcome!");
+
     }
 }
---
+++ src/com/amigoscode/TheArrays.java
@@ -0,0 +1,23 @@
+package com.amigoscode;
+
+import java.util.Arrays;
+
+
+public class TheArrays {
+    public static void main(String[] args) {
+        String[] colors = new String[5];
+        colors[0] = "purple";
+        colors[1] = "blue";
+
+        System.out.println(Arrays.toString(colors));
+
+        System.out.println(colors[0]);
+        System.out.println(colors[1]);
+        System.out.println(colors[2]);
+        System.out.println(colors[3]);
+        System.out.println(colors[4]);
+
+        colors[2] = "yellow";
+        System.out.println(colors[2]);
+    }
+}
